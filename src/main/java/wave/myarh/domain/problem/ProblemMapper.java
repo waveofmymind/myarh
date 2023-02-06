cb532b7b4f78f914ac06cfe2b09a3d05a53a001f
waveofmymind <sjun0913@gmail.com>
2023-02-06 19:36:49 +0900

    feat: Problem 엔티티 관련 매핑 구현 및 매핑 테스트 완료

--- src/main/java/wave/myarh/domain/problem/ProblemMapper.java
+++ src/main/java/wave/myarh/domain/problem/ProblemMapper.java
@@ -1,4 +1,18 @@
 package wave.myarh.domain.problem;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import wave.myarh.domain.problem.domain.Problem;
+import wave.myarh.domain.problem.dto.request.ProblemRequestDto;
+import wave.myarh.domain.problem.dto.response.ProblemResponseDto;
+
+@Mapper(componentModel = "spring") // 스프링 빈으로 등록
 public interface ProblemMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "problemTagList",ignore = true)
+    @Mapping(target = "reviewList", ignore = true)
+    Problem toEntity(ProblemRequestDto requestDto);
+
+    ProblemResponseDto toDto(Problem problem);
 }
