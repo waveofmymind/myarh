919be9038d9a281de2968b5bdbb527de6af5b86a
waveofmymind <sjun0913@gmail.com>
2023-02-06 18:27:25 +0900

    Initial commit

---
+++ src/main/java/wave/myarh/MyarhApplication.java
@@ -0,0 +1,13 @@
+package wave.myarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyarhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyarhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wave/myarh/MyarhApplicationTests.java
@@ -0,0 +1,13 @@
+package wave.myarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyarhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
