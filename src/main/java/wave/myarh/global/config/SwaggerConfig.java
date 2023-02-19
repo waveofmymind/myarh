f46b434b8f04a5153f1c1b99f5814ae77fbca536
waveofmymind <sjun0913@gmail.com>
2023-02-19 12:00:42 +0900

    [todo]

---
+++ src/main/java/wave/myarh/global/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package wave.myarh.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("데모 프로젝트 API Document")
+                .version("v0.0.1")
+                .description("데모 프로젝트의 API 명세서입니다.");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
